08de85f562f2180e79bac3e4c6a1a6c4e0d6abd8
Jadson Teixeira <jadsontxm@gmail.com>
2025-10-19 01:48:34 -0300

    Spring Boot

---
+++ api/src/main/java/med/vall/api/controller/MedicoController.java
@@ -0,0 +1,26 @@
+package med.vall.api.controller;
+
+import jakarta.validation.Valid;
+import med.vall.api.medico.DadosCadastroMedico;
+import med.vall.api.medico.Medico;
+import med.vall.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("medicos")
+public class MedicoController {
+
+    @Autowired
+    private MedicoRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
+        repository.save(new Medico(dados));
+    }
+}
